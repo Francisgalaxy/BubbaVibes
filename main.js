8ac6f728752e2ed14e2cc68eee81a88be8f5dbb4
Cindy <hrca2225i@gmail.com>
2025-05-28 10:32:55 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = fundation () {
+var replay = prompt ("Bienvenid@ ¿Cuál es tu nombre?", "" )
+altet ("Es un gusto tenerte aquí " + replay + " !" )
+}
+let miBoton = document.querySelector('button');
+miButon.onclick = funtion() {
+    altet ('Con el código: JT05 obtendrás un 5% en tu próxima compra' ); 
+}
